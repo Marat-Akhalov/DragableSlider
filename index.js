8afff19455e6135f07d5f25bfe806740aee82e1b
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-03 19:10:56 +0400

    add: add drag logic

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 'use strict';
+const container = document.querySelector('.container');
+const draggable = document.querySelector('.draggable');
+
+draggable.addEventListener('pointerdown', e => {
+  const draggableItem = e.target.closest('.draggable__item');
+
+  if (!draggableItem) return;
+
+  let leftShift = draggableItem.getBoundingClientRect().left;
+
+  draggable.addEventListener('dragstart', e => {
+    e.preventDefault();
+  });
+
+  draggable.addEventListener('pointermove', e => {
+    let newLeft = e.clientX;
+
+    draggableItem.style.left = newLeft - leftShift + 'px';
+  });
+
+  function dragStart() {}
+});
