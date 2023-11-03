db50c36bd1601c8d334d7196c87e2fc79ecc678d
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-03 18:21:05 +0400

    add: add core html elements

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 'use strict';
-
-const container = document.querySelector('.container');
-const p = document.querySelector('.text');
-
-document.addEventListener('selectionchange', e => {
-  console.log(e.target.getSelection());
-});
